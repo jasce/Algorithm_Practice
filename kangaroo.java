e0f043217afb2f44515917d0f5afee82292b763d
Jasmeet Singh <jascee.gtbit@gmail.com>
2017-06-12 12:39:15 +0530

    Refactored Kangaroo

--- kangaroo.java
+++ kangaroo.java
@@ -13,30 +13,18 @@ public class Solution {
         }
         else if (x1 == x2){
             return "YES";
-        }
-        else if (x1 > x2){
-            while(x1!=x2 && x1 > x2){
-                x1 += v1;
-                x2 += v2;
-            }
-            if(x1 == x2){
-                return "YES";
-            }
-            else{
-                return "NO";
-            }
+        }        
+        else if( v1 == v2){
+            return "NO";
         }
         else{
-             while(x1!=x2 && x2 > x1){
-                x1 += v1;
-                x2 += v2;
-            }
-            if(x1 == x2){
+            if ((Math.abs(x1-x2))%(Math.abs(v1-v2)) == 0  && v1 > v2){
                 return "YES";
             }
             else{
                 return "NO";
             }
+           
         }
         
     }
