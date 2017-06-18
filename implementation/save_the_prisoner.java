f6d6d79102af66f674c0119e01cc2f49284fd36e
Jasmeet Singh <jascee.gtbit@gmail.com>
2017-06-18 11:26:49 +0530

    Save the prisoner done

---
+++ implementation/save_the_prisoner.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int saveThePrisoner(int n, int m, int s){
+        // Complete this function
+        int answer = ((m % n) + (s-1))%n;
+        if(answer == 0)
+            return n;
+        else
+            return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            int m = in.nextInt();
+            int s = in.nextInt();
+            int result = saveThePrisoner(n, m, s);
+            System.out.println(result);
+        }
+    }
+}
