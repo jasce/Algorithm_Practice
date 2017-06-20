b7a255625d7bbeeca85e5e936f26e892ad88486c
Jasmeet Singh <jascee.gtbit@gmail.com>
2017-06-20 15:56:34 +0530

    Minimum Absolute Differnece done

---
+++ greedy/absolute_min_difference.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        int min_first = 1000000000;
+        int min_second = 100000000;
+        int last_el;
+        int index_first = 0;
+        // your code goes here
+        int min = 100000000;
+        int last;             
+        Arrays.sort(a);
+        last = a[0] < 0 ?-a[0]: a[0] ;
+         for(int i = 1 ;i <n;i++ ){
+            a[i] = a[i]<0 ? -a[i] : a[i];           //Store Absolute Values     
+             last = (a[i] - a[i-1]) < 0? a[i-1] - a[i] :  a[i] - a[i-1];
+             if(last < min)
+                 min = last;
+        } 
+        System.out.println(min);          
+    }
+}
